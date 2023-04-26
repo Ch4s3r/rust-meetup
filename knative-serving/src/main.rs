6e904c26179929fa79d8e95d56e639f02d11e0e8
Ch4s3r <lamprecht.patrick1@gmail.com>
2023-04-26 22:18:51 +0200

    Init

---
+++ knative-serving/src/main.rs
@@ -0,0 +1,20 @@
+use axum::{
+    http::StatusCode,
+    response::IntoResponse,
+    routing::{get, post},
+    Json, Router,
+};
+use serde::{Deserialize, Serialize};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(root));
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
+}
